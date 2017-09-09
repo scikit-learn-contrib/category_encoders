14d86c12ccb43657d329bd3b4514956aa07c88ef
Will McGinnis <will@predikto.com>
2017-09-09 09:32:00 -0400

    checking instantiated encoders

--- category_encoders/tests/test_estimators.py
+++ category_encoders/tests/test_estimators.py
@@ -11,13 +11,13 @@ class TestEncoders(unittest.TestCase):
     """
 
     def test_general_transformers(self):
-        check_transformer_general('hashing_encoder', HashingEncoder)
-        check_transformer_general('backward_difference_encoder', BackwardDifferenceEncoder)
-        check_transformer_general('binary_encoder', BinaryEncoder)
-        check_transformer_general('helmert_encoder', HelmertEncoder)
-        check_transformer_general('ordinal_encoder', OrdinalEncoder)
-        check_transformer_general('polynomial_encoder', PolynomialEncoder)
-        check_transformer_general('sum_coding', SumEncoder)
-        check_transformer_general('one_hot', OneHotEncoder)
-        check_transformer_general('basen', BaseNEncoder)
-        check_transformer_general('leave_one_out',LeaveOneOutEncoder)
+        check_transformer_general('hashing_encoder', HashingEncoder())
+        check_transformer_general('backward_difference_encoder', BackwardDifferenceEncoder())
+        check_transformer_general('binary_encoder', BinaryEncoder())
+        check_transformer_general('helmert_encoder', HelmertEncoder())
+        check_transformer_general('ordinal_encoder', OrdinalEncoder())
+        check_transformer_general('polynomial_encoder', PolynomialEncoder())
+        check_transformer_general('sum_coding', SumEncoder())
+        check_transformer_general('one_hot', OneHotEncoder())
+        check_transformer_general('basen', BaseNEncoder())
+        check_transformer_general('leave_one_out', LeaveOneOutEncoder())
