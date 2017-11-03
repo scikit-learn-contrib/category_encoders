8669f5b487b2e285771330d0ecfc166ca43b5e9a
Will McGinnis <will@predikto.com>
2017-11-03 12:35:54 -0400

    release prep

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 from codecs import open
 from os import path
 
-__version__ = '1.2.4'
+__version__ = '1.2.5'
 
 
 here = path.abspath(path.dirname(__file__))
