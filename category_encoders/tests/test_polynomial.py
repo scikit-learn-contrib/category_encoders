a7a32b690d8f1951f866b9a4f837414b975a9774
Jan Motl <jan@motl.us>
2018-10-08 17:58:41 +0200

    Full import path

--- category_encoders/tests/test_polynomial.py
+++ category_encoders/tests/test_polynomial.py
@@ -1,10 +1,8 @@
-import math
-import numpy as np
 import pandas as pd
 from unittest2 import TestCase  # or `from unittest import ...` if on Python 3.4+
 
 import category_encoders as encoders
-from test_utils import deep_round
+from category_encoders.tests.test_utils import deep_round
 
 a_encoding = [1, -0.7071067811865476, 0.40824829046386313]
 b_encoding = [1, -5.551115123125783e-17, -0.8164965809277261]
