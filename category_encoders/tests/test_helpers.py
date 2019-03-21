e8475dd21f4199b68cfad3add82294c78346b336
florian <datarian@againstthecurrent.ch>
2019-03-21 13:00:55 +0100

    Added test case for categories

--- category_encoders/tests/test_helpers.py
+++ category_encoders/tests/test_helpers.py
@@ -34,3 +34,9 @@ class TestHelpers(TestCase):
 
         X = np.ones([5, 5], dtype='float64')
         verify_numeric(pd.DataFrame(X))
+        
+    def test_verify_raises_AssertionError_on_categories(self):
+        # Categories should raise AssertionError
+        X = pd.DataFrame([['a', 'b', 'c'], ['d', 'e', 'f']], dtype='category')
+        with self.assertRaises(Exception):
+            verify_numeric(pd.DataFrame(X))
