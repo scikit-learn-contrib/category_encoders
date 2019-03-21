89cf1fa97cfbd7e72b480fa90e2c7d357533c3d8
florian <datarian@againstthecurrent.ch>
2019-03-21 10:28:24 +0100

    Added unittest for binary encoder inside sklearn.ColumnTransformer

--- category_encoders/tests/test_binary.py
+++ category_encoders/tests/test_binary.py
@@ -1,6 +1,8 @@
 import pandas as pd
 from unittest2 import TestCase  # or `from unittest import ...` if on Python 3.4+
 import numpy as np
+import random
+from sklearn.compose import ColumnTransformer
 
 import category_encoders as encoders
 
@@ -28,3 +30,23 @@ class TestBinaryEncoder(TestCase):
 
         # make sure they are the same
         self.assertTrue(split.equals(c))
+
+    def test_binary_return_missing_in_column_transformer(self):
+        
+        random.seed(2001)
+        ds = [[random.choice(['A', 'B', 'C', np.nan])] for row in range(1000)]
+
+        df = pd.DataFrame(ds, columns=['categorical_na'])
+        df['categorical_na'] = pd.Categorical(df['categorical_na'], categories=['A', 'B', 'C'])
+        
+        X = df
+        expected_na = X.loc[:,'categorical_na'].isna().sum()
+        
+        ct = ColumnTransformer([
+            ("be",
+             encoders.BinaryEncoder(handle_missing="return_nan"),
+             ["categorical_na"])
+        ])
+        X_trans = ct.fit_transform(X)
+        result_na = X.shape[0] - X_trans[~np.isnan(X_trans).all(axis=1)].shape[0]
+        self.assertEqual(expected_na, result_na)
