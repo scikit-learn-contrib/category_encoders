2dc7536969404623099354fafbe5ba4348315027
SLLiu <slliu96@163.com>
2019-06-05 21:44:01 +0800

    Add Multi-Process Supported HashingEncoder

--- category_encoders/__init__.py
+++ category_encoders/__init__.py
@@ -10,6 +10,7 @@ from category_encoders.backward_difference import BackwardDifferenceEncoder
 from category_encoders.binary import BinaryEncoder
 from category_encoders.count import CountEncoder
 from category_encoders.hashing import HashingEncoder
+from category_encoders.nhashing import NHashingEncoder
 from category_encoders.helmert import HelmertEncoder
 from category_encoders.one_hot import OneHotEncoder
 from category_encoders.ordinal import OrdinalEncoder
@@ -31,6 +32,7 @@ __all__ = [
     'BackwardDifferenceEncoder',
     'BinaryEncoder',
     'HashingEncoder',
+    'NHashingEncoder',
     'HelmertEncoder',
     'OneHotEncoder',
     'OrdinalEncoder',
