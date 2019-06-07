b8aca3a6a06f8aa8a5cd9ca69e68447c81492bf6
SLLiu <slliu96@163.com>
2019-06-07 18:32:12 +0800

    Update hashing.py

--- category_encoders/hashing.py
+++ category_encoders/hashing.py
@@ -318,19 +318,18 @@ class HashingEncoder(BaseEstimator, TransformerMixin):
 
         """
 
-        # Do the check in transform()
-        # if self._dim is None:
-        #     raise ValueError('Must train encoder before it can be used to transform data.')
-        #
-        # # first check the type
-        # X = util.convert_input(X)
-        #
-        # # then make sure that it is the right size
-        # if X.shape[1] != self._dim:
-        #     raise ValueError('Unexpected input dimension %d, expected %d' % (X.shape[1], self._dim, ))
-        #
-        # if not self.cols:
-        #     return X
+        if self._dim is None:
+            raise ValueError('Must train encoder before it can be used to transform data.')
+
+        # first check the type
+        X = util.convert_input(X)
+
+        # then make sure that it is the right size
+        if X.shape[1] != self._dim:
+            raise ValueError('Unexpected input dimension %d, expected %d' % (X.shape[1], self._dim, ))
+
+        if not self.cols:
+            return X
 
         X = self.hashing_trick(X, hashing_method=self.hash_method, N=self.n_components, cols=self.cols)
 
