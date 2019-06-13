e16948c635847501e299b6c8dd474ec5a1e98350
SLLiu <slliu96@163.com>
2019-06-13 13:56:21 +0800

    Revert "Update document"
    This reverts commit 1e98bb84b66294b4838cadd8c4a0abf768b85c14.

--- examples/benchmarking_cpu/benchmarking_cpu.py
+++ examples/benchmarking_cpu/benchmarking_cpu.py
@@ -1,43 +1,27 @@
-import psutil
 import time
-import pandas as pd
 import numpy as np
+import psutil
+import pandas as pd
 import multiprocessing
-import category_encoders as encoders
 import category_encoders.tests.helpers as th
+import category_encoders as encoders
 
-__author__ = 'LiuShulun'
 
-
-"""
-Benchmarking of cpu utilization during every encoding.
-Record the peak value and average value.
-Commonly, the value is below 50 because of the core distribution by system.
-HashingEncoder
-
-TODO:
-differentiate between average and peak
-differentiate between training and scoring
-"""
-
-# benchmarking result format
-result_cols = ['encoder', 'X_shape', 'average_time (s)', 'average_cpu_utilization (%)']
-results = []
-cpu_utilization = multiprocessing.Manager().Queue()
-
+# data definitions
 np_X = th.create_array(n_rows=10000)
 np_y = np.random.randn(np_X.shape[0]) > 0.5
 X = th.create_dataset(n_rows=10000)
 X_t = th.create_dataset(n_rows=5000, extras=True)
 
 
+result_cols = ['encoder', 'X_shape', 'average_time (s)', 'average_cpu_utilization (%)']
+
+results = []
+
+cpu_utilization = multiprocessing.Manager().Queue()
+
 
 def get_cpu_utilization():
-    """
-    new process for recording cpu utilization
-    record cpu utilization every 0.2 second & calculate its mean value
-    the value is the cpu utilization during every encoding
-    """
     global cpu_utilization
     psutil.cpu_percent(None)
     while True:
