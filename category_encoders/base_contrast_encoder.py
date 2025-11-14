c8dfd70c80e1f015b769b515cf796c2fff384e7a
Josh Soref <2119212+jsoref@users.noreply.github.com>
2025-11-13 20:22:14 -0500

    spelling: handle
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- category_encoders/base_contrast_encoder.py
+++ category_encoders/base_contrast_encoder.py
@@ -124,7 +124,7 @@ class BaseContrastEncoder(util.UnsupervisedTransformerMixin, util.BaseEncoder):
         handle_missing: str
             How to handle missing values.
         handle_unknown: str
-            How to hande unkown values.
+            How to handle unkown values.
 
         Returns
         -------
