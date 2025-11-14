15945c24033079b56c209e9ff2d361ed19ec3e0f
Josh Soref <2119212+jsoref@users.noreply.github.com>
2025-11-13 20:21:36 -0500

    spelling: label
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- category_encoders/leave_one_out.py
+++ category_encoders/leave_one_out.py
@@ -161,7 +161,7 @@ class LeaveOneOutEncoder( util.SupervisedTransformerMixin,util.BaseEncoder):
     def transform_leave_one_out(self, X: pd.DataFrame, y: pd.Series | None, mapping=None):
         """Apply leave-one-out-encoding to a dataframe.
 
-        If a target is given the lable-mean is calculated without the target (left out).
+        If a target is given the label-mean is calculated without the target (left out).
         Otherwise, the label mean from the fit step is taken.
         """
         random_state_ = check_random_state(self.random_state)
