de2d996832522c2b5f3ee4f617ee0115e8f56892
Obvious <obvious@obvious.ai>
2026-09-07 18:09:02 +0000

    chore: prep 2.11.0 release (changelog + version bump)

--- category_encoders/__init__.py
+++ category_encoders/__init__.py
@@ -43,7 +43,7 @@ def __getattr__(name):
         return GLMMEncoder
     raise AttributeError(f'module {__name__!r} has no attribute {name!r}')
 
-__version__ = '2.10.0'
+__version__ = '2.11.0'
 
 __author__ = 'willmcginnis', 'cmougan', 'paulwestenthanner'
 
